9aafe533ad03b287a52877a65ebb6ebf72ca2b58
Savio <saviomariano77@gmail.com>
2024-08-15 09:02:28 -0300

    adicionando multiplicação, divisão, e potenciação

--- codigos/aula02_aritimetica.py
+++ codigos/aula02_aritimetica.py
@@ -1,5 +1,6 @@
 # Operações aritemeticas
 # Somar, subtrair, multiplicar, dividir.
+
 somar = 8 + 7
 print(somar)
 
@@ -16,3 +17,24 @@ print(subtracao)
 subtracao = 6 - 15
 
 print(subtracao)
+
+multiplicacao = 3 * 6
+
+print(multiplicacao)
+
+divisao = 10 / 4
+
+print(divisao)
+
+# divisão com resultado inteiro
+divisao_inteira = 10 // 4 # o // serve para trazer o resultado inteiro
+                          # o // não arrendonda o resultado, traz somente inteiro.
+print(divisao_inteira)
+
+resto = 25 % 7
+
+print(resto)
+
+potenciacao = 3 ** 2
+
+print(potenciacao)
