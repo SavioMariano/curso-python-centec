53b8e83fcf4bc5135128865cbd4cac77fd8b66f4
Savio <saviomariano77@gmail.com>
2024-08-19 18:44:20 -0300

    Corrigindo falhas da calculadora de imc

--- codigos/q1_calculadora_imc.py
+++ codigos/q1_calculadora_imc.py
@@ -13,16 +13,16 @@ print("Seu IMC é:\n> ", imc, "\n\n")
 if imc <= 18.5:
     print("Abaixo do peso")
     
-elif imc >= 18.5 and imc <= 24.9:
+elif imc >= 18.5 and imc < 25:
     print("Peso normal")
     
-elif imc >= 25 and imc <= 29.9:
+elif imc >= 25 and imc < 30:
     print("Sobrepeso")
     
-elif imc >= 30 and imc <= 34.9:
+elif imc >= 30 and imc < 35:
     print("Obesidade Grau I")
 
-elif imc >= 35 and imc <= 39.9:
+elif imc >= 35 and imc < 40:
     print("Obseidade Grau II")
     
 elif imc >= 40:
