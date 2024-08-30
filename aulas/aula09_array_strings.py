913c1e3a61b181c39c49c20de1e37bdc809f67a0
Savio <saviomariano77@gmail.com>
2024-08-30 18:07:36 -0300

    enviando mais arquivos tanto de aulas tanto como desafios

---
+++ aulas/aula09_array_strings.py
@@ -0,0 +1,21 @@
+lista_numeros = [9, 4, 7, 10, 12, 6]
+
+
+# 0 - 5: 0, 1, 2, 3, 4, 5
+# indice: 0
+# pega valor pelo índice (cria transitividade)
+for indice in range(6):
+    valor = lista_numeros[indice]
+    print(valor)
+
+
+# pega valor diretamento do array
+for valor in lista_numeros:
+    print(valor)
+
+
+texto = 'um texto qualquer'
+
+
+for caractere in texto:
+    print(caractere)
---
+++ desafio_logica/q3_contar_vogais.py
@@ -0,0 +1,10 @@
+
+texto = input('digite seu texto: ')
+contador = 0    
+
+for c in texto:
+    if c == "A" or c == "a" or c == "E" or c == "e" or c == "I" or c == "I" or c == "O" or c == "o" or c == "U" or c == "u":         
+        contador += 1
+        print(contador)
+    
+
